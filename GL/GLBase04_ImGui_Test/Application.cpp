2bc884c367990539cd834bcca991e0c0a01b362f
s.inoue <s.inoue@otwo.jp>
2022-05-23 15:32:43 +0900

    add

---
+++ GL/GLBase04_ImGui_Test/Application.cpp
@@ -0,0 +1,127 @@
+#pragma once
+
+#include	"Application.h"
+
+using namespace Sample;
+
+//描画関連
+CameraPtr camera;
+
+/**
+ * @brief		コンストラクタ
+ */
+Application::Application()
+: Framework() {
+}
+
+/**
+ * @brief		デストラクタ
+ */
+Application::~Application() {
+}
+
+/**
+ * @brief		初期化
+ */
+void Application::Initialize() {
+    //リソースディレクトリを素材配置先に指定
+    ::SetCurrentDirectory(L"Resources");
+
+    //TODO:
+    //アプリの初期化処理を記述
+
+    //カメラ設定
+    camera = std::make_shared<Camera>();
+    camera->Create2D(1024, 768);
+    GraphicsController::GetInstance().Camera(camera);
+
+    auto& imguiIO = ImGui::GetIO();
+    imguiIO.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+}
+
+/**
+ * @brief		更新
+ */
+void Application::Update() {
+	//TODO:
+	//アプリの更新処理を記述
+
+    /**/
+    unsigned flags = ImGuiWindowFlags_NoTitleBar;
+    flags |= ImGuiWindowFlags_NoMove;
+    flags |= ImGuiWindowFlags_NoResize;
+    flags |= ImGuiWindowFlags_NoBackground;
+    flags |= ImGuiWindowFlags_MenuBar;
+    ImGui::Begin("##BaseWindowDockSpace", nullptr, flags);
+    {
+        ImGui::SetWindowPos(ImVec2{ 0.0f, 0.0f });
+        ImGui::SetWindowSize(ImVec2{ (float)GraphicsController::GetInstance().ScreenWidth(), (float)GraphicsController::GetInstance().ScreenHeight() });
+        ImGuiID baseDockspace = ImGui::GetID("##BaseDockSpace");
+        unsigned dockNodeFlags = ImGuiDockNodeFlags_None;
+        dockNodeFlags |= ImGuiWindowFlags_NoBackground;
+        ImGui::DockSpace(baseDockspace, ImVec2{ 0.0f, 0.0f }, dockNodeFlags);
+    }
+    ImGui::End();
+    /*//**/
+    //ImGui::ShowDemoWindow();
+
+    ImGui::Begin("canvas");
+    {
+
+    }
+    ImGui::End();
+
+    ImGui::Begin("animations");
+    {
+
+    }
+    ImGui::End();
+
+    ImGui::Begin("tool bar");
+    {
+
+    }
+    ImGui::End();
+
+    ImGui::Begin("sprites");
+    {
+
+    }
+    ImGui::End();
+
+    ImGui::Begin("animation view");
+    {
+
+    }
+    ImGui::End();
+
+    ImGui::Begin("editor");
+    {
+
+    }
+    ImGui::End();
+}
+
+/**
+ * @brief		描画
+ */
+void Application::Render() {
+
+    //初期設定と画面クリア
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClearDepth(1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    //TODO:
+    //アプリの描画処理を記述
+
+
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    //ターゲットのリセット
+    GraphicsController::GetInstance().ResetTarget();
+}
---
+++ GL/GLBase04_ImGui_Test/Framework/Graphics/GraphicsController.h
@@ -0,0 +1,108 @@
+#pragma once
+
+#include	"../Common/Common.h"
+#include	"../Common/Singleton.h"
+#include	"Camera.h"
+
+namespace Sample {
+
+    // FWD
+    class FrameBuffer;
+    class Sprite;
+    class Shader;
+    using FrameBufferPtr = std::shared_ptr<FrameBuffer>;
+    using SpritePtr      = std::shared_ptr<Sprite>;
+    using ShaderPtr      = std::shared_ptr<Shader>;
+
+	/**
+	 * @brief		描画管理
+	 */
+	class GraphicsController : public Singleton<GraphicsController>
+	{
+		friend class Singleton<GraphicsController>;
+	private:
+		/** ウインドウ */
+		GLFWwindow*									window_;
+		/** メインカメラ */
+		CameraPtr									currentCamera_;
+		/**	画面幅 */
+		int											width_;
+		/**	画面高さ */
+		int											height_;
+        /** 描画用バッファ */
+        FrameBufferPtr                              frameBuffer_;
+        /** 描画用スプライト */
+        SpritePtr                                   frameSprite_;
+        /** シェーダー */
+        ShaderPtr                                   shader_;
+
+		GraphicsController()
+		: window_(nullptr)
+		, currentCamera_(nullptr)
+		, width_(0)
+		, height_(0)
+		, frameBuffer_(nullptr)
+		, frameSprite_(nullptr)
+        , shader_(nullptr) {
+		}
+		virtual ~GraphicsController() = default;
+	public:
+		/**
+		 * @brief		初期化
+		 */
+        void Initialize(GLFWwindow* w);
+
+        /**
+         * @brief		描画用バッファの生成
+         */
+        void CreateBuffer();
+
+		/**
+		 * @brief		ターゲットのリセット
+		 */
+		void ResetTarget() {
+			glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
+			glViewport(0, 0, width_, height_);
+		}
+
+        /**
+         * @brief		描画開始
+         */
+        void RenderBegin();
+
+        /**
+         * @brief		描画
+         */
+        void RenderEnd();
+
+		/**
+		 * @brief		利用中のカメラの設定
+		 * @param[in]	cam		利用中のカメラ
+		 */
+		void Camera(CameraPtr& cam) {
+			currentCamera_ = cam;
+		}
+
+		/**
+		 * @brief		利用中のカメラの取得
+		 * @return		利用中のカメラ
+		 */
+		const CameraPtr& Camera() const {
+			return currentCamera_;
+		}
+
+		/**
+		 * @brief		画面
+		 */
+		GLFWwindow* Window() const noexcept { return window_; }
+		/**
+		 * @brief		画面幅の取得
+		 */
+		const GLint ScreenWidth() const noexcept { return width_; }
+		/**
+		 * @brief		画面高さの取得
+		 */
+		const GLint ScreenHeight() const noexcept { return height_; }
+	};
+
+}
